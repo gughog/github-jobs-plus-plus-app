30dc79b597a334bbf2dca6626fa0688d429c9360
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-09-16 20:18:45 -0300

    feat: added conciliation method to sync fetched data and async storage arrays

--- src/helpers/formatters.helpers.ts
+++ src/helpers/formatters.helpers.ts
@@ -1,4 +1,7 @@
 import {formatDistance} from 'date-fns';
+import {Job} from '../types';
+import {AlertWrapper} from '../helpers/modals.helpers';
+import {getApplied, getFavorites} from '../services/Storage.service';
 
 /**
  * Formats a date to a textual date distance indicator.
@@ -9,4 +12,44 @@ import {formatDistance} from 'date-fns';
  */
 export const formatDate = (date: string) => {
   return formatDistance(new Date(date), new Date(), {addSuffix: true});
+};
+
+export const reconcileArraysAndReturnWithFlags = async (jobs: Job[]) => {
+  try {
+    const JobsCopy = jobs || [];
+    const favoriteJobsCopy = (await getFavorites()) || [];
+    const jobsAppliedCopy = (await getApplied()) || [];
+
+    for (let i = 0; i < JobsCopy.length; i += 1) {
+      for (let j = 0; j < favoriteJobsCopy.length; j += 1) {
+        if (
+          JobsCopy[i].id === favoriteJobsCopy[j].id &&
+          favoriteJobsCopy[j].isFavorite
+        ) {
+          JobsCopy[i].isFavorite = true;
+        }
+      }
+      for (let j = 0; j < jobsAppliedCopy.length; j += 1) {
+        if (
+          JobsCopy[i].id === jobsAppliedCopy[j].id &&
+          jobsAppliedCopy[j].applied
+        ) {
+          JobsCopy[i].applied = true;
+        }
+      }
+    }
+
+    return JobsCopy;
+  } catch (error) {
+    AlertWrapper({
+      title: 'Some error has occured!',
+      description: `The following error has occured: ${error.message}`,
+      okayButton: {
+        text: 'Ok',
+        action: () => {},
+      },
+    });
+
+    return jobs;
+  }
 };
