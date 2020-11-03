94f856cfbd060a4449e1b52c5638f67a120127ea
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-11-03 22:52:36 +0100

    feat: search screen feature added

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,6 +1,6 @@
 // Here it will export all (and only) views.
 import {HomeScreen} from './Home';
-import {SearchScreen} from './Search';
+import {SearchScreen} from './SearchScreen';
 import {FavoritesScreen} from './Favorites';
 
 export {HomeScreen, SearchScreen, FavoritesScreen};
